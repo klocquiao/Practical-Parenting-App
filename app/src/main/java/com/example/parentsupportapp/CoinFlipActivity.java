2eb41343725baaa7826fbe53d47690a96c6a5ef3
klocq <knl5@sfu.ca>
2021-11-02 23:18:54 -0700

    Implement interpolation for coin flip animation to make it feel more dynamic, add randomization logic.
    The coin will now "bounce" at the start of the animation via interpolation. Furthermore, animation time is adjusted using Random in order to retain animation smoothness while also making the flip random.

--- app/src/main/java/com/example/parentsupportapp/CoinFlipActivity.java
+++ app/src/main/java/com/example/parentsupportapp/CoinFlipActivity.java
@@ -6,19 +6,33 @@ import android.animation.Animator;
 import android.animation.AnimatorInflater;
 import android.animation.AnimatorListenerAdapter;
 import android.animation.AnimatorSet;
+import android.animation.ObjectAnimator;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.View;
+import android.view.animation.AccelerateInterpolator;
+import android.view.animation.AnticipateInterpolator;
+import android.view.animation.AnticipateOvershootInterpolator;
+import android.view.animation.BounceInterpolator;
+import android.view.animation.DecelerateInterpolator;
 import android.widget.Button;
 import android.widget.ImageView;
+
+import java.util.Random;
 
 public class CoinFlipActivity extends AppCompatActivity {
 
     private Animator coinFlip1Animation;
     private Animator coinFlip2Animation;
     private AnimatorSet coinFlipAnimation;
+    private ObjectAnimator transitionUpHead;
+    private ObjectAnimator transitionUpTail;
+    private AnimatorSet transitionUpAnimation;
+    private ObjectAnimator transitionDownHead;
+    private ObjectAnimator transitionDownTail;
+    private AnimatorSet transitionDownAnimation;
 
     private Handler handler;
     private ImageView head;
@@ -28,11 +42,16 @@ public class CoinFlipActivity extends AppCompatActivity {
     private boolean isHead;
     private boolean isFlipping;
 
+    private Random random;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_coin_flip);
         isHead = true;
+
+        random = new Random();
 
         head = findViewById(R.id.head);
         tail = findViewById(R.id.tail);
@@ -48,11 +67,26 @@ public class CoinFlipActivity extends AppCompatActivity {
                 R.animator.coin_flip_animator_2);
 
         coinFlipAnimation = new AnimatorSet();
+        transitionUpAnimation = new AnimatorSet();
+        transitionDownAnimation = new AnimatorSet();
+
+        transitionUpHead = ObjectAnimator.ofFloat(head, "translationY", -250f);
+        transitionUpTail = ObjectAnimator.ofFloat(tail, "translationY", -250f);
+        transitionUpAnimation.playTogether(transitionUpHead, transitionUpTail);
+        transitionUpAnimation.setInterpolator(new AnticipateOvershootInterpolator());
+        transitionUpAnimation.setDuration(600);
+
+        transitionDownHead = ObjectAnimator.ofFloat(head, "translationY", 0f);
+        transitionDownTail = ObjectAnimator.ofFloat(tail, "translationY", 0f);
+        transitionDownAnimation.playTogether(transitionDownHead, transitionDownTail);
+        transitionDownAnimation.setInterpolator(new BounceInterpolator());
+        transitionDownAnimation.setDuration(1000);
+        transitionDownAnimation.setStartDelay(600);
 
         head.setCameraDistance(8000);
         tail.setCameraDistance(8000);
 
-        coinFlipAnimation.addListener(new AnimatorListenerAdapter() {
+       coinFlipAnimation.addListener(new AnimatorListenerAdapter() {
             @Override
             public void onAnimationEnd(Animator animation) {
                 if (isFlipping == true) {
@@ -77,16 +111,35 @@ public class CoinFlipActivity extends AppCompatActivity {
         flipButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                int delay;
                 headOrTail();
                 coinFlipAnimation.play(coinFlip1Animation).with(coinFlip2Animation);
+                transitionUpAnimation.start();
+                transitionDownAnimation.start();
                 coinFlipAnimation.start();
+
+                if (random.nextBoolean()) {
+                    transitionUpAnimation.setDuration(600);
+                    transitionDownAnimation.setStartDelay(600);
+                    transitionDownAnimation.setDuration(1000);
+                    delay = 1200;
+
+                }
+                else {
+                    transitionUpAnimation.setDuration(700);
+                    transitionDownAnimation.setStartDelay(700);
+                    transitionDownAnimation.setDuration(1200);
+                    delay = 1400;
+                }
 
                 handler.postDelayed(new Runnable() {
                     @Override
                     public void run() {
                         coinFlipAnimation.cancel();
+                        head.setRotationX(0);
+                        tail.setRotationX(0);
                     }
-                }, 2500);
+                }, delay);
 
             }
         });
