8854ca9fd45d6e854f6f0105d29ceb12e59a9216
Mohammed <mishalm@sfu.ca>
2021-11-05 12:39:13 -0700

    main menu implemented

--- app/src/main/java/com/example/parentsupportapp/MainActivity.java
+++ app/src/main/java/com/example/parentsupportapp/MainActivity.java
@@ -1,19 +1,56 @@
 package com.example.parentsupportapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.parentsupportapp.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
-    private ActivityMainBinding binding;
+
+    private Button configureChildButton;
+    private Button flipButton;
+    private Button timerButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        com.example.parentsupportapp.databinding.ActivityMainBinding binding = ActivityMainBinding.inflate(getLayoutInflater());
+
         setContentView(binding.getRoot());
         setSupportActionBar(binding.toolbar);
+
+        this.configureChildButton = (Button) findViewById(R.id.menuConfigureChild);
+        this.flipButton = (Button) findViewById(R.id.menuFlip);
+        this.timerButton = (Button) findViewById(R.id.menuTimer);
+
+        this.setupButtonListeners();
     }
+
+    private void setupButtonListeners() {
+        this.configureChildButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, ChildConfigActivity.class));
+            }
+        });
+
+        this.flipButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, CoinFlipActivity.class));
+            }
+        });
+
+        this.timerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, TimerActivity.class));
+            }
+        });
+    }
+
 }
