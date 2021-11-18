aa430f65168c4acc747d0c60c881bbabb8761cb0
Damir Paripovic <paripovicdamir@gmail.com>
2021-11-17 23:11:43 -0800

    implemented add edit and remove tasks activities

--- app/src/main/java/com/example/parentsupportapp/tasksConfig/RemoveTaskActivity.java
+++ app/src/main/java/com/example/parentsupportapp/tasksConfig/RemoveTaskActivity.java
@@ -4,7 +4,9 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import com.example.parentsupportapp.R;
 
@@ -14,6 +16,14 @@ public class RemoveTaskActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_remove_task);
+        Toolbar toolbar = findViewById(R.id.toolbarRemoveTask);
+        setSupportActionBar(toolbar);
+        ActionBar actionbar = getSupportActionBar();
+        if (actionbar != null) {
+            actionbar.setDisplayHomeAsUpEnabled(true);
+            actionbar.setTitle("Remove Task");
+        }
+
     }
 
     public static Intent makeIntent(Context context) {
