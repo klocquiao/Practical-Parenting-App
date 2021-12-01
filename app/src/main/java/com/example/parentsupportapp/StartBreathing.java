e2f0e243070d0b57707314bc39e7f2d7ccebaab0
yma60 <yma60@sfu.ca>
2021-12-01 15:23:46 -0800

    Add start breathing activity

---
+++ app/src/main/java/com/example/parentsupportapp/StartBreathing.java
@@ -0,0 +1,14 @@
+package com.example.parentsupportapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class StartBreathing extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start_breathing);
+    }
+}
