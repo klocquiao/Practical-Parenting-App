a89d7ee41501cfb8a6791cd45e000befe03e11b5
klocq <knl5@sfu.ca>
2021-12-06 15:07:07 -0800

    Implement shared preferences.
    Option for # of breaths during the breathing activity will now save after the activity is stopped and will be reloaded upon reopening.

--- app/src/main/java/com/example/parentsupportapp/StartBreathingActivity.java
+++ app/src/main/java/com/example/parentsupportapp/StartBreathingActivity.java
@@ -4,13 +4,19 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.example.parentsupportapp.model.PriorityQueue;
+import com.google.gson.Gson;
+
 public class StartBreathingActivity extends AppCompatActivity {
+    private static final String BREATHING_PREF = "BreathingActivityPref";
+    private static final String BREATHING_KEY = "BreathingActivityKey";
     private Button startButton;
     private EditText etNumBreaths;
     private Button btnIncrease;
@@ -22,14 +28,22 @@ public class StartBreathingActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start_breathing);
 
+        numBreaths = getBreathingActivityPrefs(this);
         etNumBreaths = findViewById(R.id.etNumBreaths);
+        etNumBreaths.setText(Integer.toString(numBreaths));
+
         btnIncrease = findViewById(R.id.btnAddBreath);
         btnDecrease = findViewById(R.id.btnDecreaseBreath);
-        numBreaths = Integer.parseInt(etNumBreaths.getText().toString());
 
         setupButtonListeners();
         setupIncreaseBreathBtn();
         setupDecreaseBreathBtn();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        saveBreathingActivityPrefs(this);
     }
 
     private void setupDecreaseBreathBtn() {
@@ -38,7 +52,7 @@ public class StartBreathingActivity extends AppCompatActivity {
             public void onClick(View v) {
                 if (numBreaths > 1) {
                     numBreaths--;
-                    etNumBreaths.setText("" + numBreaths);
+                    etNumBreaths.setText(Integer.toString(numBreaths));
                 }
                 else if (numBreaths == 1) {
                     Toast.makeText(StartBreathingActivity.this, "Number of breaths cannot be less than 1!", Toast.LENGTH_SHORT).show();
@@ -53,7 +67,7 @@ public class StartBreathingActivity extends AppCompatActivity {
             public void onClick(View v) {
                 if (numBreaths <= 9) {
                     numBreaths++;
-                    etNumBreaths.setText("" + numBreaths);
+                    etNumBreaths.setText(Integer.toString(numBreaths));
                 }
                 else if (numBreaths == 10) {
                     Toast.makeText(StartBreathingActivity.this, "Number of breaths cannot be more than 10!", Toast.LENGTH_SHORT).show();
@@ -72,6 +86,18 @@ public class StartBreathingActivity extends AppCompatActivity {
                 startActivity(breathingIntent);
             }
         });
+    }
+
+    public static Integer getBreathingActivityPrefs(Context context) {
+        SharedPreferences prefs = context.getSharedPreferences(BREATHING_PREF, MODE_PRIVATE);
+        return prefs.getInt(BREATHING_KEY, 1);
+    }
+
+    private void saveBreathingActivityPrefs(Context context) {
+        SharedPreferences prefs = context.getSharedPreferences(BREATHING_PREF, MODE_PRIVATE);
+        SharedPreferences.Editor editor = prefs.edit();
+        editor.putInt(BREATHING_KEY, numBreaths);
+        editor.apply();
     }
 
     public static Intent makeIntent(Context c) {
